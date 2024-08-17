9e7e48dd842f699229056e35dfd26c6b94aac18f
VerbOperative <vrbtn@vrb.is>
2024-08-17 01:19:47 -0400

    updated schema to allow the client to update clicks but not target url and updated client to be able to redirect prior to dealing with the admin page and authentication for creating a shortened url.

--- urlshort/index.php
+++ urlshort/index.php
@@ -36,7 +36,7 @@
             <h2>Get'cher URL Shortener here!</h2>
             <p>Fill out the below form to create a shortened URL.</p>
             <form action="/s/" method="POST">
-              <label for="url">URL:</label>
+              <label for="url" id="labelforurl">URL:</label>
               <input type="url" id="url" name="url" required />
               <button id="submiturl" type="submit">Shorten</button>
           </section>
@@ -55,6 +55,78 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="/pocketbasesdk/js-sdk-0.21.4/dist/pocketbase.umd.js"></script>
     <script type="text/javascript">
+
+      function create_and_show_modal(id, labelId, content, closeEvent, deleteEvent) {
+        const modal = document.createElement('div');
+            modal.classList.add('modal', 'fade');
+            modal.setAttribute('id', id);
+            modal.setAttribute('tabindex', '-1');
+            modal.setAttribute('role', 'dialog');
+            modal.setAttribute('aria-labelledby', labelId);
+            modal.setAttribute('aria-hidden', 'true');
+
+            const modalDialog = document.createElement('div');
+            modalDialog.classList.add('modal-dialog');
+            modalDialog.setAttribute('role', 'document');
+
+            const modalContent = document.createElement('div');
+            modalContent.classList.add('modal-content');
+
+            const modalHeader = document.createElement('div');
+            modalHeader.classList.add('modal-header');
+
+            const modalTitle = document.createElement('h5');
+            modalTitle.classList.add('modal-title');
+            modalTitle.setAttribute('id', labelId);
+            modalTitle.innerText = 'Admin Page';
+
+            const modalBody = document.createElement('div');
+            modalBody.classList.add('modal-body');
+            //modalBody.innerHTML = 'This is the admin page content.';
+            modalBody.innerHTML = content;
+
+            const modalFooter = document.createElement('div');
+            modalFooter.classList.add('modal-footer');
+
+            // Add a left aligned delete button
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('btn', 'btn-danger');
+            deleteButton.setAttribute('type', 'button');
+            deleteButton.innerText = 'Delete';
+            deleteButton.addEventListener('click', async (e) => {
+              if (deleteEvent) {
+                deleteEvent(e, id);
+              }
+            });
+
+            const closeButton = document.createElement('button');
+            closeButton.classList.add('btn', 'btn-secondary');
+            // ensure button is left aligned:
+            closeButton.style.marginLeft = 'auto';
+            closeButton.setAttribute('type', 'button');
+            closeButton.setAttribute('data-dismiss', 'modal');
+            closeButton.innerText = 'Close';
+            closeButton.addEventListener('click', async (e) => {
+              if (closeEvent) {
+                closeEvent(e, id);
+              }
+            });
+
+            modalFooter.appendChild(deleteButton);
+            modalFooter.appendChild(closeButton);
+            modalHeader.appendChild(modalTitle);
+            modalContent.appendChild(modalHeader);
+            modalContent.appendChild(modalBody);
+            modalContent.appendChild(modalFooter);
+            modalDialog.appendChild(modalContent);
+            modal.appendChild(modalDialog);
+            document.body.appendChild(modal);
+
+            $('#'+id).modal('show');
+      }
+
+    </script>
+    <script type="text/javascript">
       
       function get_random_string(length) {
         if (!length) {
@@ -69,10 +141,13 @@
       }
 
       const pb = new PocketBase("https://vrb.is/s");
+      
+      //pb.collection("URL").authWithOAuth2(authConfig);
       //console.log(pb);
       const submiturl = document.getElementById("submiturl");
       submiturl.addEventListener("click", async (e) => {
         e.preventDefault();
+        
         const url = document.getElementById("url").value;
 
         // check key is unique
@@ -120,6 +195,7 @@
 
       
       window.addEventListener("load", async () => {
+        /////////////// REDIRECT
         // get the key from the URL, issue redirect after incrementing the click count
         let key = window.location.pathname;
         key = key.substring(1)
@@ -155,6 +231,33 @@
           console.log("no key found in URL");
         }
 
+        /////////////// ADMIN PAGE
+        if (!pb.authStore.isValid) {
+          // disable form
+          document.getElementById('url').disabled = true;
+          document.getElementById('submiturl').disabled = true;
+          // OAuth2 authentication with a single realtime call.
+          //
+          // Make sure to register https://vrb.is/s/api/oauth2-redirect as redirect url.
+          const authData = await pb.collection('users').authWithOAuth2({ provider: 'discord' });
+          console.log(pb.authStore.token);
+          console.log(pb.authStore.model.id);
+        }
+        
+        if (pb.authStore.isValid) {
+          // enable form
+          document.getElementById('url').disabled = false;
+          document.getElementById('submiturl').disabled = false;
+          // add a logout btton
+          const logoutButton = document.createElement('button');
+          logoutButton.classList.add('btn', 'btn-danger');
+          logoutButton.innerText = 'Logout';
+          logoutButton.addEventListener('click', async (e) => {
+            pb.authStore.clear();
+            window.location.reload();
+          });
+          document.getElementById('labelforurl').insertAdjacentElement( 'beforebegin'  , logoutButton);
+        }
 
         if (key.includes('admin')) {
           // get the admin key from the URL, issue redirect to the admin page
@@ -169,48 +272,25 @@
             console.log(record);
 
             // Display the admin page using bootstrap modals
-            const modal = document.createElement('div');
-            modal.classList.add('modal', 'fade');
-            modal.setAttribute('id', 'adminModal');
-            modal.setAttribute('tabindex', '-1');
-            modal.setAttribute('role', 'dialog');
-            modal.setAttribute('aria-labelledby', 'adminModalLabel');
-            modal.setAttribute('aria-hidden', 'true');
-
-            const modalDialog = document.createElement('div');
-            modalDialog.classList.add('modal-dialog');
-            modalDialog.setAttribute('role', 'document');
-
-            const modalContent = document.createElement('div');
-            modalContent.classList.add('modal-content');
-
-            const modalHeader = document.createElement('div');
-            modalHeader.classList.add('modal-header');
-
-            const modalTitle = document.createElement('h5');
-            modalTitle.classList.add('modal-title');
-            modalTitle.setAttribute('id', 'adminModalLabel');
-            modalTitle.innerText = 'Admin Page';
-
-            const modalBody = document.createElement('div');
-            modalBody.classList.add('modal-body');
-            //modalBody.innerHTML = 'This is the admin page content.';
-            modalBody.innerHTML = '<p>Original URL: <a href="' + record.target + '">' + record.target + '</a></p><p>Shortened URL: <a href="https://vrb.is/'+record.key+'">https://vrb.is/'+record.key+'</a></p><p>Clicks: ' + record.clicks + '</p>';
-
-            const modalFooter = document.createElement('div');
-            modalFooter.classList.add('modal-footer');
-
-            // Add a left aligned delete button
-            const deleteButton = document.createElement('button');
-            deleteButton.classList.add('btn', 'btn-danger');
-            deleteButton.setAttribute('type', 'button');
-            deleteButton.innerText = 'Delete';
-            deleteButton.addEventListener('click', async () => {
+            //'<p>Original URL: <a href="' + record.target + '">' +
+            //record.target + '</a></p><p>Shortened URL: <a
+            //href="https://vrb.is/'+record.key+'">https://vrb.is/'+record.key+'</a></p><p>Clicks:
+            //' + record.clicks + '</p>'
+            const content = `
+              <p>URL: <a href="${record.target}">${record.target}</a></p>
+              <p>Shortened URL: <a href="https://vrb.is/${record.key}">https://vrb.is/${record.key}</a></p>
+              <p>Clicks: ${record.clicks}</p>
+              <p>Admin Key: ${record.admin_key}</p>
+            `;
+            
+            create_and_show_modal('adminModal', 'adminModalLabel', content, async (e, id)=>{
+              $('#' + id).modal('hide');
+            }, async (e, id)=>{
               try {
                 record.is_active = false;
                 const result = await pb.collection('URL').update(record.id, record);
                 // close modal and redirect to home page
-                $('#adminModal').modal('hide');
+                $('#' + id).modal('hide');
                 console.log('deleted record and redirecting to home');
                 window.location.href = '/';
               } catch (e) {
@@ -218,35 +298,11 @@
               }
             });
 
-            const closeButton = document.createElement('button');
-            closeButton.classList.add('btn', 'btn-secondary');
-            // ensure button is left aligned:
-            closeButton.style.marginLeft = 'auto';
-            closeButton.setAttribute('type', 'button');
-            closeButton.setAttribute('data-dismiss', 'modal');
-            closeButton.innerText = 'Close';
-            closeButton.addEventListener('click', () => {
-              $('#adminModal').modal('hide');
-            });
-
-            modalFooter.appendChild(deleteButton);
-            modalFooter.appendChild(closeButton);
-            modalHeader.appendChild(modalTitle);
-            modalContent.appendChild(modalHeader);
-            modalContent.appendChild(modalBody);
-            modalContent.appendChild(modalFooter);
-            modalDialog.appendChild(modalContent);
-            modal.appendChild(modalDialog);
-            document.body.appendChild(modal);
-
             const alert = document.createElement('div');
             alert.classList.add('alert', 'alert-warning');
             alert.setAttribute('role', 'alert');
             alert.innerHTML = 'Shortened URL: <a href="https://vrb.is/'+record.key+'">https://vrb.is/'+record.key+'</a> ( Admin Key for stats/deletion: <a href="https://vrb.is/admin/' + record.admin_key + '">' + record.admin_key + '</a> )';
             document.body.appendChild(alert);
-
-            // Show the admin modal
-            $('#adminModal').modal('show');
 
           } catch (e) {
             console.error(e);
